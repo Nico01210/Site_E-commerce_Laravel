a3fc1c2b37c5e80629145a330a7a652344adab99
Perret <nicolas.perret@le-campus-numerique.fr>
2025-07-07 14:27:50 +0200

    route html

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <h1>Bienvenue sur la Homepage</h1>
+
+    <p> Voici la page d'acceuil du site </p>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Homepage";
-});
-
-Route::get('/product', function () {
-    return "Liste des produits";
-});
-
-Route::get('/product/{id}', function ($id) {
-    return "Fiche du produit " . $id;
-})->where('id', '[0-9]+');
-
-Route::get('/cart', function () {
-    return "Panier";
+    return view('home');
 });
