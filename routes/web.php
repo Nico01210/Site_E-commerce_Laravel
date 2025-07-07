e9724b132199e7ff28b40c8c51c7cb3693524500
Perret <nicolas.perret@le-campus-numerique.fr>
2025-07-07 11:59:07 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-  return 'Hello World From Laravel v' . app()->version() . ' (PHP v' . PHP_VERSION . ')';
+    return "Homepage";
+});
+
+Route::get('/product', function () {
+    return "Liste des produits";
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit " . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/cart', function () {
+    return "Panier";
 });
