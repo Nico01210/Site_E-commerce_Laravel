05be64f3f94a750e558bf37f752611b537f726c7
Perret <nicolas.perret@le-campus-numerique.fr>
2025-07-21 10:24:06 +0200

    ajout favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 </head>
 <body>
 
