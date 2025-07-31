a4bca5294cc520ef3908ada2bd32b0029667f201
Perret <nicolas.perret@le-campus-numerique.fr>
2025-07-31 14:12:31 +0200

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 </head>
